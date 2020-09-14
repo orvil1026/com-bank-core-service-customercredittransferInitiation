51837f9b2972ad0934df4b53c8b6714f224772d6
dsilvamarsh <dsilvamarsh@gmail.com>
2020-09-14 16:31:52 +0530

    inital commit

---
+++ src/main/java/com/tutorial/launcher/Application.java
@@ -0,0 +1,47 @@
+package com.tutorial.launcher;
+
+import java.text.NumberFormat;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.tutorial.controller","com.tutorial.exception"})
+@EnableJpaRepositories(basePackages = {"com.tutorial.repository"})
+@EntityScan(basePackages = {"com.tutorial.beans"})
+@EnableTransactionManagement
+
+public class Application {
+	private static final Logger logger = LogManager.getLogger(Application.class.getName());
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+		Runtime runtime = Runtime.getRuntime();
+
+		final NumberFormat format = NumberFormat.getInstance();
+
+		
+		final long maxMemory = runtime.maxMemory();
+		final long allocatedMemory = runtime.totalMemory();
+		final long freeMemory = runtime.freeMemory();
+		final long mb = 1024 * 1024;
+		final String mega = " MB";
+
+		logger.info("========================== Memory Info ==========================");
+		logger.info("Free memory: " + format.format(freeMemory / mb) + mega);
+		logger.info("Allocated memory: " + format.format(allocatedMemory / mb) + mega);
+		logger.info("Max memory: " + format.format(maxMemory / mb) + mega);
+		logger.info("Total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / mb) + mega);
+		logger.info("=================================================================\n");
+	}
+
+	
+	
+}
